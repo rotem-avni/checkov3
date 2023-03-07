9ac99d87919cfd1e459e9f86989c4720c066a1b5
pazbechor <pbechor@paloaltonetworks.com>
2023-03-07 18:19:25 +0200

    .

---
+++ tests/sast/source_code/javascript/EncryptUsingSalt/crypto_builtin_hkdf_sync/fail2.js
@@ -0,0 +1,18 @@
+// Node.js program to demonstrate the
+// crypto.hkdf() function
+
+// Importing crypto module
+const crypto = require('crypto')
+
+// getting derived key
+// by using hkdf() method
+var salt = ''
+const val = crypto.hkdfSync('sha512', 'key', salt,
+						'info', 64, (err, derivedKey) => {
+
+	// checking if any error is found
+	if (err) throw err;
+
+	// display the result
+	console.log(Buffer.from(derivedKey).toString('hex'));
+});
