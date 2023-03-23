4ccaa5428fda85658cc7f3c501342406986bc09b
Omry Mendelovich <omry155@gmail.com>
2023-03-23 13:49:04 +0200

    more tests

--- checkov/sast/go_runner.py
+++ checkov/sast/go_runner.py
@@ -3,18 +3,23 @@ import json
 from os import path
 from typing import Dict, Any
 
+AVAILABLE_LANGUAGES = {'python'}
+
 current_dir = path.dirname(path.realpath(__file__))
 library = ctypes.cdll.LoadLibrary(path.join(current_dir, '../sast_core/library.so'))
 analyze_code = library.analyzeCode
 analyze_code.restype = ctypes.c_void_p
 
 
-def run_go_library(source_code_dir: str = '',
-                   policy_dir: str = '',
-                   language: str = '') -> Dict[str, Any]:
+def run_go_library(source_code_file: str,
+                   source_code_dir: str,
+                   policy_file: str,
+                   policy_dir: str,
+                   language: str) -> Dict[str, Any]:
+    validate_params(**locals())
     document = {
         "source_code_dir": source_code_dir,
-         "source_code_file": source_code_file,
+        "source_code_file": source_code_file,
         "policy_dir": policy_dir,
         "policy_file": policy_file,
         "language": language,
@@ -27,5 +32,19 @@ def run_go_library(source_code_dir: str = '',
 
     # convert our byte array to a string
     entire_file_string = entire_file_bytes.decode('utf-8')
-    print(entire_file_output, entire_file_bytes, entire_file_string)
     return json.loads(entire_file_string)
+
+
+def validate_params(source_code_file: str,
+                    source_code_dir: str,
+                    policy_file: str,
+                    policy_dir: str,
+                    language: str):
+    if not source_code_file and not source_code_dir:
+        raise Exception('must provide source code file or dir for sast runner')
+    if not policy_dir and not policy_file:
+        raise Exception('must provide policy file or dir for sast runner')
+    if not language:
+        raise Exception('must provide a language for sast runner')
+    if language not in AVAILABLE_LANGUAGES:
+        raise Exception(f'currently only support {AVAILABLE_LANGUAGES}')
--- tests/sast/test_go_runner.py
+++ tests/sast/test_go_runner.py
@@ -12,7 +12,44 @@ class GoRunnerTest(unittest.TestCase):
             proc = subprocess.Popen(['make', 'build'], cwd=sast_core_dir)  # nosec
             proc.wait()
 
-    def testSomthing(self):
+    def test_validation_pass(self):
         from checkov.sast.go_runner import run_go_library
-        res = run_go_library('some_dir')
+        res = run_go_library(source_code_dir='somedir',
+                             source_code_file='',
+                             policy_dir='somedir',
+                             policy_file='',
+                             language='python')
         assert isinstance(res, dict)
+
+    def test_validation_fail_no_policy(self):
+        from checkov.sast.go_runner import run_go_library
+        with self.assertRaises(Exception):
+            run_go_library(source_code_dir='somedir',
+                           source_code_file='',
+                           policy_dir='',
+                           policy_file='',
+                           language='python')
+
+    def test_validation_fail_no_code(self):
+        from checkov.sast.go_runner import run_go_library
+        with self.assertRaises(Exception):
+            run_go_library(source_code_dir='',
+                           source_code_file='',
+                           policy_dir='pol',
+                           policy_file='',
+                           language='python')
+
+    def test_validation_fail_lang(self):
+        from checkov.sast.go_runner import run_go_library
+        with self.assertRaises(Exception):
+            run_go_library(source_code_dir='somedir',
+                           source_code_file='',
+                           policy_dir='somedir',
+                           policy_file='',
+                           language='')
+        with self.assertRaises(Exception):
+            run_go_library(source_code_dir='somedir',
+                           source_code_file='',
+                           policy_dir='somedir',
+                           policy_file='',
+                           language='js')
