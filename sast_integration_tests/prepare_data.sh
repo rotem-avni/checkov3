8e007c1a6fb35dc51dacb523d6cddf5e4a3ae3f2
Rotem Avni <ravni@paloaltonetworks.com>
2023-07-19 11:27:56 +0300

    use BC_API_KEY and remove python 3.7

--- sast_integration_tests/prepare_data.sh
+++ sast_integration_tests/prepare_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pipenv run checkov -s --framework sast_python -d flask --bc-api-key $BC_KEY -o json > checkov_report_sast_python.json
-pipenv run checkov -s --framework sast_java -d jenkins --bc-api-key $BC_KEY -o json > checkov_report_sast_java.json
-pipenv run checkov -s --framework sast_javascript -d axios --bc-api-key $BC_KEY -o json > checkov_report_sast_javascript.json
+pipenv run checkov -s --framework sast_python -d flask -o json > checkov_report_sast_python.json
+pipenv run checkov -s --framework sast_java -d jenkins -o json > checkov_report_sast_java.json
+pipenv run checkov -s --framework sast_javascript -d axios -o json > checkov_report_sast_javascript.json
