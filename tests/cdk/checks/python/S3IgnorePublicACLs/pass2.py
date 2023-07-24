3d1bcdb8bf47decf7beb395c0434e3376bf6c47f
Taylor <28880387+tsmithv11@users.noreply.github.com>
2023-07-23 22:42:12 -0700

    Create pass2.py

---
+++ tests/cdk/checks/python/S3IgnorePublicACLs/pass2.py
@@ -0,0 +1,24 @@
+from constructs import Construct
+from aws_cdk import App, Stack 
+from aws_cdk import (
+    aws_s3 as s4
+)
+
+class MyStac2(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+		# pass
+        bucket = s4.Bucket(self, "MyS3Bucket",
+            bucket_name='my-s3-bucket',
+            public_read_access=False,
+            block_public_access=s4.BlockPublicAccess(
+                ignore_public_acls=True
+            )
+        )
+
+app = App()
+MyStack(app, "my-stack-name")
+
+app.synth()
