943e3283e4d7c310dbe41e0f5de002010f7026b8
Taylor <28880387+tsmithv11@users.noreply.github.com>
2023-07-23 22:42:40 -0700

    Create fail2.py

---
+++ tests/cdk/checks/python/S3IgnorePublicACLs/fail2.py
@@ -0,0 +1,25 @@
+from constructs import Construct
+from aws_cdk import App, Stack 
+from aws_cdk import (
+    aws_s3 as s4
+)
+
+class MyStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+        
+        value = False
+		# fail
+        bucket = s4.Bucket(self, "MyS3Bucket",
+            bucket_name='my-s3-bucket',
+            public_read_access=False,
+            block_public_access=s4.BlockPublicAccess(
+                ignore_public_acls=value
+            )
+        )
+
+app = App()
+MyStack(app, "my-stack-name")
+
+app.synth()
