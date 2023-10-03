78efc34fb90ebdfda5776160f67031f4a189417e
arielk <ar.kruszyn@gmail.com>
2023-10-03 17:16:31 +0300

    cdk integration tests step fix

--- cdk_integration_tests/prepare_data.sh
+++ cdk_integration_tests/prepare_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python checkov -s --framework sast_python -o json \
+pipenv run checkov -s --framework sast_python -o json \
     -d cdk_integration_tests/src/python/S3BucketEncryption \
     --external-checks-dir checkov/cdk/checks/python/S3BucketEncryption.yaml > checkov_sast_report_S3BucketEncryption.json
