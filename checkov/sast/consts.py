8adba9351dc97e0061fbdfd83882d2ee100fbe9e
arielk <ar.kruszyn@gmail.com>
2023-10-03 16:35:51 +0300

    remove sast consts

--- checkov/sast/consts.py
+++ checkov/sast/consts.py
@@ -1,7 +1,6 @@
 from enum import Enum
 from typing import List, Any, Set
 
-from semgrep.constants import RuleSeverity
 from checkov.common.graph.checks_infra.enums import Operators
 
 
@@ -17,44 +16,6 @@ class SastLanguages(Enum):
     PYTHON = 'python'
     JAVA = 'java'
     JAVASCRIPT = 'javascript'
-
-
-class SastEngines(str, Enum):
-    def __str__(self) -> str:
-        return self.value
-
-    SEMGREP = 'semgrep'
-    PRISMA = 'prisma cloud'
-
-
-class SemgrepAttribute(str, Enum):
-    def __str__(self) -> str:
-        return self.value
-
-    ID = 'id'
-    MESSAGE = 'message'
-    SEVERITY = 'severity'
-    CWE = 'cwe'
-    OWASP = 'owasp'
-    LANGUAGES = 'languages'
-    PATTERNS = 'patterns'
-    PATTERN = 'pattern'
-    PATTERN_EITHER = 'pattern-either'
-    PATTERN_INSIDE = 'pattern-inside'
-    PATTERN_NOT_INSIDE = 'pattern-not-inside'
-    PATTERN_NOT = 'pattern-not'
-    PATTERN_REGEX = 'pattern-regex'
-    PATTERN_NOT_REGEX = 'pattern-not-regex'
-    METAVARIABLE = 'metavariable'
-    METAVARIABLE_PATTERN = 'metavariable-pattern'
-    METAVARIABLE_REGEX = 'metavariable-regex'
-    METAVARIABLE_COMPARISON = 'metavariable-comparison'
-    COMPARISON = 'comparison'
-    REGEX = 'regex'
-    PATTERN_SOURCES = 'pattern-sources'
-    PATTERN_SINKS = 'pattern-sinks'
-    PATTERN_SANITIZERS = 'pattern-sanitizers'
-    PATTERN_PROPAGATORS = 'pattern-propagators'
 
 
 class BqlVersion(str, Enum):
@@ -112,50 +73,6 @@ class BqlV2ConditionType(str, Enum):
     PROPAGATORS = 'propagators'
     COMPARISON = 'comparison'
 
-
-BQLV2_KEY_TO_SEMGREP_ATTR = {
-    str(BqlV2ConditionType.PATTERN): str(SemgrepAttribute.PATTERN),
-    str(BqlV2ConditionType.SOURCE): str(SemgrepAttribute.PATTERN),
-    str(BqlV2ConditionType.SINK): str(SemgrepAttribute.PATTERN),
-    str(BqlV2ConditionType.SANITIZER): str(SemgrepAttribute.PATTERN),
-    str(BqlV2ConditionType.PROPAGATOR): str(SemgrepAttribute.PATTERN),
-    str(BqlV2ConditionType.REGEX): str(SemgrepAttribute.PATTERN_REGEX),
-    str(BqlV2ConditionType.NOT_PATTERN): str(SemgrepAttribute.PATTERN_NOT),
-    str(BqlV2ConditionType.NOT_REGEX): str(SemgrepAttribute.PATTERN_NOT_REGEX),
-    str(BqlV2ConditionType.WITHIN): str(SemgrepAttribute.PATTERN_INSIDE),
-    str(BqlV2ConditionType.NOT_WITHIN): str(SemgrepAttribute.PATTERN_NOT_INSIDE),
-}
-
-BQLV2_METAVAR_KEY_TO_SEMGREP_ATTR = {
-    str(BqlV2ConditionType.PATTERN): str(SemgrepAttribute.METAVARIABLE_PATTERN),
-    str(BqlV2ConditionType.PATTERNS): str(SemgrepAttribute.METAVARIABLE_PATTERN),
-    str(BqlV2ConditionType.REGEX): str(SemgrepAttribute.METAVARIABLE_REGEX),
-    str(BqlV2ConditionType.COMPARISON): str(SemgrepAttribute.METAVARIABLE_COMPARISON)
-}
-
-PATTERN_OPERATOR_TO_SEMGREP_ATTR = {
-    Operators.EQUALS: SemgrepAttribute.PATTERN.value,
-    Operators.NOT_EQUALS: SemgrepAttribute.PATTERN_NOT.value,
-    Operators.REGEX_MATCH: SemgrepAttribute.PATTERN_REGEX.value,
-    Operators.NOT_REGEX_MATCH: SemgrepAttribute.PATTERN_NOT_REGEX.value
-}
-
-VARIABLE_OPERATOR_TO_SEMGREP_ATTR = {
-    Operators.REGEX_MATCH: SemgrepAttribute.METAVARIABLE_REGEX.value,
-    Operators.PATTERN_MATCH: SemgrepAttribute.METAVARIABLE_PATTERN.value,
-    Operators.EQUALS: SemgrepAttribute.METAVARIABLE_COMPARISON.value,
-    Operators.NOT_EQUALS: SemgrepAttribute.METAVARIABLE_COMPARISON.value,
-    Operators.GREATER_THAN: SemgrepAttribute.METAVARIABLE_COMPARISON.value,
-    Operators.GREATER_THAN_OR_EQUAL: SemgrepAttribute.METAVARIABLE_COMPARISON.value,
-    Operators.LESS_THAN: SemgrepAttribute.METAVARIABLE_COMPARISON.value,
-    Operators.LESS_THAN_OR_EQUAL: SemgrepAttribute.METAVARIABLE_COMPARISON.value
-}
-
-FILTER_OPERATOR_TO_SEMGREP_ATTR = {
-    Operators.WITHIN: SemgrepAttribute.PATTERN_INSIDE.value,
-    Operators.NOT_WITHIN: SemgrepAttribute.PATTERN_NOT_INSIDE.value,
-}
-
 SUPPORT_FILE_EXT = {
     SastLanguages.PYTHON: ['py'],
     SastLanguages.JAVA: ['java'],
@@ -184,18 +101,6 @@ COMPARISON_VALUE_TO_SYMBOL = {
     Operators.GREATER_THAN_OR_EQUAL: '>=',
     Operators.LESS_THAN: '<',
     Operators.LESS_THAN_OR_EQUAL: '<='
-}
-
-SEMGREP_SEVERITY_TO_CHECKOV_SEVERITY = {
-    RuleSeverity.ERROR: 'HIGH',
-    RuleSeverity.WARNING: 'MEDIUM',
-    RuleSeverity.INFO: 'LOW',
-}
-
-CHECKOV_SEVERITY_TO_SEMGREP_SEVERITY = {
-    'HIGH': RuleSeverity.ERROR.value,
-    'MEDIUM': RuleSeverity.WARNING.value,
-    'LOW': RuleSeverity.INFO.value,
 }
 
 POLICIES_ERRORS = 'policies_errors'
