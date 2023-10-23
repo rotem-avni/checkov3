7617715421e3e1851b50e7dfac84e29f98f48334
Mike Urbanski <murbanski@paloaltonetworks.com>
2023-10-23 09:58:42 -0500

    improve arg description

--- checkov/common/util/ext_argument_parser.py
+++ checkov/common/util/ext_argument_parser.py
@@ -328,7 +328,7 @@ class ExtArgumentParser(configargparse.ArgumentParser):
         )
         self.add(
             "--repo-id",
-            help="Identity string of the repository, with form <repo_owner>/<repo_name>. Required when using the platform integration.",
+            help="Identity string of the repository, with form <repo_owner>/<repo_name>. Required when using the platform integration (API key).",
         )
         self.add(
             "-b",
